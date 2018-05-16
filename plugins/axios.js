d411eeb5474ab3e20cc94aef14acac63490a3bc0
MinuteReversal <114233763@qq.com>
2018-05-16 17:42:30 +0800

    globle error popup message design

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,8 +3,13 @@ import axios from "axios"
 axios.interceptors.response.use(
     response => response,
     error => {
-        console.log("error");
-        $nuxt.$children[0].$alert(error.response.data.error.message);
+        $nuxt.$children[0]
+            .$message({
+                showClose: true,
+                message: error.response.data.error.message,
+                duration:10000,
+                type: 'error'
+            });
         return Promise.reject(error);
     }
 )
