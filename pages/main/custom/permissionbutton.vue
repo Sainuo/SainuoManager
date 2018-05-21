96b85e9baa588a9757850caaf433aa57831a1fc4
MinuteReversal <114233763@qq.com>
2018-05-21 17:57:28 +0800

    permission button

---
+++ pages/main/custom/permissionbutton.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <el-button @click="onShow">显示</el-button>
+    </div>
+</template>
+<script>
+export default {
+    methods:{
+        onShow(){
+            this.$alert("show");
+        }
+    }
+}
+</script>
