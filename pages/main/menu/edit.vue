051a2bb93985456d471f6f994a1b748274c4043b
MinuteReversal <114233763@qq.com>
2018-06-17 22:49:12 +0800

    menu detail

--- pages/main/menu/edit.vue
+++ pages/main/menu/edit.vue
@@ -25,11 +25,11 @@ export default {
     onConfirm() {
       var me = this;
       if(me.id === 0){
-        axios.post(apiConfig.organization_create, me.ruleForm).then(response => {
+        axios.post(apiConfig.menu_create, me.ruleForm).then(response => {
           me.$emit("confirm", response.data.result);
         });
       }else{
-        axios.put(apiConfig.organization_update, me.ruleForm).then(response => {
+        axios.put(apiConfig.menu_update, me.ruleForm).then(response => {
           me.$emit("confirm", response.data.result);
         });
       }
@@ -37,7 +37,7 @@ export default {
     loadData() {
       var me = this;
       me.loading=true
-      axios.get(apiConfig.organization_get_by_id, { params:{ organizationId: me.id }})
+      axios.get(apiConfig.menu_read, { params:{ id: me.id }})
       .then(response => {
         me.ruleForm = response.data.result;
       })
